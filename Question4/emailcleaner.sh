67223591f7779468b7ed98e508f02ca82ea5b57b
sksatyam793 <sksatyam544@gmail.com>
2026-02-05 13:56:42 +0530

    Add files via upload

--- Question4/emailcleaner.sh
+++ Question4/emailcleaner.sh
@@ -5,4 +5,3 @@ grep -E "^[a-zA-Z0-9]+@[a-zA-Z]+\.com$" emails.txt | sort | uniq > valid.txt
 
 # Extract invalid email addresses and store in invalid.txt
 grep -Ev "^[a-zA-Z0-9]+@[a-zA-Z]+\.com$" emails.txt > invalid.txt
-
