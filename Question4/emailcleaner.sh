e3a66a6dcdf642ef874236b724c3160b09d0bf05
sksatyam793 <sksatyam544@gmail.com>
2026-02-05 12:18:52 +0530

    Update emailcleaner.sh

--- Question4/emailcleaner.sh
+++ Question4/emailcleaner.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+
+# Extract valid email addresses, remove duplicates, and store in valid.txt
+grep -E "^[a-zA-Z0-9]+@[a-zA-Z]+\.com$" emails.txt | sort | uniq > valid.txt
+
+# Extract invalid email addresses and store in invalid.txt
+grep -Ev "^[a-zA-Z0-9]+@[a-zA-Z]+\.com$" emails.txt > invalid.txt
 
